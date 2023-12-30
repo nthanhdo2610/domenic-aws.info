8bd67d51fdc44762cc7160707681f97d15fd038a
nthanhdo2610 <nthanhdo11110030@gmail.com>
2023-12-30 15:13:51 -0800

    update and deploy aws s3

--- src/common/AwsAccess.js
+++ src/common/AwsAccess.js
@@ -1,6 +1,6 @@
 const AwsAccess = {
-    apiPath: "https://a4iizsy098.execute-api.us-west-2.amazonaws.com/prod",
-    // apiPath: "https://api.domenic-aws.info",
+    // apiPath: "https://a4iizsy098.execute-api.us-west-2.amazonaws.com/prod",
+    apiPath: "https://api.domenic-aws.info",
 }
 
 export default AwsAccess;
